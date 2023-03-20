7c5d8cd20e8431724a05325997d1847ad55aae99
bbenshalom <bbenshalom@outbrain.com>
2023-03-20 12:44:01 +0200

    just work already

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,7 @@ impl Predictor {
 impl ConcurrentPredictor {
     unsafe fn predict(&mut self, input_buffer: &str) -> f32 {
         self.thread_pool.install(|| {
-            let mut predictor = self.predictors[rayon::current_thread_index().unwrap()];
+            let &mut predictor = &self.predictors[rayon::current_thread_index().unwrap()];
             predictor.predict(input_buffer)
         })
     }
